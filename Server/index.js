5091d985608d7e9a1b48553d52b9dcc7289fb16c
Ashish kumar pathak <ashishpathak0933@gmail.com>
2023-12-24 23:10:26 +0100

    first commit

---
+++ Server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL, {   
+}).then(()=> {
+    console.log(`db connection successfully`)
+}).catch((err)=>{
+    console.log(err.message)
+});
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server is running on ${process.env.PORT}`)
+});
+
