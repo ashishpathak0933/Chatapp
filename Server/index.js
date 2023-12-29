a3ba61cf0725798363eda2c7bf67955c515241ea
Ashish kumar pathak <ashishpathak0933@gmail.com>
2023-12-29 22:25:38 +0100

    updates

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const userRoutes = require("./routes/userRoutes")
-
+PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err.message)
 });
 
-PORT = 5000;
+
 app.listen(PORT, () =>
   console.log(`Server started on ${PORT}`)
 );
